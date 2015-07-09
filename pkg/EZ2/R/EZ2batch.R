e474abfbe1bc4f7795a0c8f77986beee941555ff
raoul (Dr ) <rgrasman@uva.nl>
2015-07-09 12:14:33 +0000

    Cleaned up ERRORS/NOTES/WARNINGS. Extensively rewrote the package .Rd file. Updated the references section in the .Rd files.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rtmodels@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/EZ2/R/EZ2batch.R
+++ pkg/EZ2/R/EZ2batch.R
@@ -4,12 +4,14 @@ function (pstart, ObsValPair, ..., data, nrestart = 1, method = "Nelder-Mead",
 {
     mdl = c(ObsValPair, ...)
     t(apply(data, 1, function(x) {
-        attach(as.list(x), warn = FALSE)
-        fit <- EZ2(pstart, mdl, method = method, control = control, 
-            hessian = hessian)
-        for (i in 1:nrestart) fit <- EZ2(fit$par, mdl, method = method, 
-            control = control, hessian = hessian)
-        unlist(fit)
+        attach(as.list(x), warn.conflicts = FALSE) 
+        # with(as.list(x), { # This was an attempt to remove the attach statement... but it lead to an execution ERROR in R CMD check
+          fit <- EZ2(pstart, mdl, method = method, control = control, 
+              hessian = hessian)
+          for (i in 1:nrestart) fit <- EZ2(fit$par, mdl, method = method, 
+              control = control, hessian = hessian)
+          unlist(fit)
+        # })
     }))
 }
 
